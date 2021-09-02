a70a98fdb4163c803488142fc2bf28b816785652
Elie <eliemhess@gmail.com>
2021-09-02 09:14:04 -0700

    First draft

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+use sha2::{Sha256, Digest};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: hash [INPUT TYPE] [INPUT]");
+        return;
+    }
+
+    match args[1].as_str() {
+        "-f" | "--file" => { handle_file(&args[2]); },
+        "-r" | "--raw" => { handle_raw(&args[2]); },
+        _ => { eprintln!("Acceptable input types:\n\tRaw contents of next arg (-r, --raw)\n\tFrom file (-f, --file)"); }
+    }
+}
+
+fn handle_file(filename: &str) {
+    match read_file(filename) {
+        Ok(contents) => { println!("{}", hash(contents)); },
+        Err(e) => { eprintln!("Error reading file: {}", e); }
+    };    
+}
+
+fn handle_raw(input: &str) {
+    println!("{}", hash(input));
+}
+
+fn read_file(filename: &str) -> std::io::Result<Vec<u8>> {
+    let mut file = fs::File::open(filename)?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn hash(input: impl AsRef<[u8]>) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    return hasher
+        .finalize()
+        .into_iter()
+        .map(|x| if x < 16 { format!("0{:x}", x) } else { format!("{:x}", x) })
+        .collect();
+}
