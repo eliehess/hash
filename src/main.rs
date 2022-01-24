b07f11c66d43901396a2f053fd682f7237093522
eliehess <eliemhess@gmail.com>
2022-01-24 15:30:41 -0500

    Simplify input logic

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,61 @@
 use std::{env, fs, io::{self, Read}};
 use hash::{Hasher, Sha256, Sha512};
 
-enum InputType { Raw, File }
-
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    enum InputType { Raw, File }
 
     let mut input_type: Option<InputType> = Option::None;
     let mut hasher: Option<Box<dyn Hasher>> = Option::None;
     let mut input: Option<String> = Option::None;
 
-    match args.len() {
-        2 => { 
-            input_type = Some(InputType::Raw);
-            input = Some((&args[1]).to_string());
-        },
-        3 => match args[1].as_str() {
-            "-r" | "--raw" => { 
-                input_type = Some(InputType::Raw);
-                input = Some((&args[2]).to_string());
-            },
-            "-f" | "--file" => { 
-                input_type = Some(InputType::File);
-                input = Some((&args[2]).to_string());
-            },
-            _ => { panic!("Acceptable input types:\n\tRaw contents of next arg (-r, --raw)\n\tFrom file (-f, --file)"); }
-        },
-        5 => {
-            match args[1].as_str() {
-                "-r" | "--raw" => { 
-                    input_type = Some(InputType::Raw);
-                    input = Some((&args[2]).to_string());
-                },
-                "-f" | "--file" => { 
-                    input_type = Some(InputType::File);
-                    input = Some((&args[2]).to_string());
-                },
-                "-t" | "--type" => match args[2].as_str() {
-                    "256" | "sha256" => { hasher = Some(Box::new(Sha256)) },
-                    "512" | "sha512" => { hasher = Some(Box::new(Sha512)) },
-                    _ => { panic!("Acceptable hash types: sha256, sha512"); }
-                },
-                _ => { panic!("Acceptable input types:\n\tRaw contents of next arg (-r, --raw)\n\tFrom file (-f, --file)") }
-            }
-
-            match args[3].as_str() {
-                "-r" | "--raw" => { 
-                    match input_type {
-                        Some(_) => panic!("Input type entered twice"),
-                        None => { 
-                            input_type = Some(InputType::Raw);
-                            input = Some((&args[4]).to_string()); }
+    let mut i = 1;
+    while i < args.len() {
+        match args[i].as_str() {
+            "-r" | "--raw" => {
+                match input_type {
+                    Some(_) => panic!("Must specify -r/--raw or -f/--file only once"),
+                    None => {
+                        input_type = Some(InputType::Raw);
+                        input = Some((&args[i+1]).to_string());
+                        i += 1;
                     }
-                },
-                "-f" | "--file" => { 
-                    match input_type {
-                        Some(_) => panic!("Input type entered twice"),
-                        None => { 
-                            input_type = Some(InputType::File);
-                            input = Some((&args[4]).to_string()); }
+                }
+            },
+            "-f" | "--file" => {
+                match input_type {
+                    Some(_) => panic!("Must specify -r/--raw or -f/--file only once"),
+                    None => {
+                        input_type = Some(InputType::File);
+                        input = Some((&args[i+1]).to_string());
+                        i += 1;
                     }
-                },
-                "-t" | "--type" => match hasher {
-                    Some(_) => panic!("Hash type entered twice"),
-                    None =>  match args[4].as_str() {
-                        "256" | "sha256" => { hasher = Some(Box::new(Sha256)) },
-                        "512" | "sha512" => { hasher = Some(Box::new(Sha512)) },
+                }
+            },
+            "-t" | "--type" => match hasher {
+                Some(_) => panic!("Hash type entered twice"),
+                None => { 
+                    hasher = match args[i+1].as_str() {
+                        "256" | "sha256" => { Some(Box::new(Sha256)) },
+                        "512" | "sha512" => { Some(Box::new(Sha512)) },
                         _ => { panic!("Acceptable hash types: sha256, sha512"); }
-                    },
-                }               
-                _ => { panic!("Acceptable input types:\n\tRaw contents of next arg (-r, --raw)\n\tFrom file (-f, --file)") }
-            }
-        },
-        _ => { panic!("Usage: hash <INPUT TYPE> [INPUT]"); }
+                    };
+                    i += 1;
+                },
+            },
+            _ => {
+                match input_type {
+                    Some(_) => panic!("Unrecognized argument: {}", args[i]),
+                    None => {
+                        input_type = Some(InputType::Raw);
+                        input = Some((&args[i]).to_string());
+                    }
+                }
+            }  
+        }
+
+        i += 1;
     }
 
     let input_type: InputType = input_type.unwrap_or_else(|| panic!("No input type specified"));
